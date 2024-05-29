53fa266b82f4f29c0927e922c51d36e2e5041d62
dima chupris <dimachupris@gmail.come>
2024-05-29 21:25:27 +0300

    Init index.html and script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+function sayHi() {
+    alert("Hello from Git!");
+}
+
+function work(n) {
+    alert(`Work,work ${n} times harder!`);
+}
+
+function sayBye() {
+    alert("Good bye from Git!");
+} 
+
